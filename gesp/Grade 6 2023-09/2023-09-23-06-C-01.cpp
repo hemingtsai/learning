e0e692193357099f6e293a2a16596e8c52831791
hemingtsai <rarmicrozip@outlook.com>
2024-12-01 10:56:30 +0800

    完成202309 06第一题

--- gesp/Grade 6 2023-09/2023-09-23-06-C-01.cpp
+++ gesp/Grade 6 2023-09/2023-09-23-06-C-01.cpp
@@ -1,4 +1,6 @@
+#include <climits>
 #include <iostream>
+#include <vector>
 
 int main() {
   int N, L;
@@ -6,9 +8,23 @@ int main() {
   struct Drink {
     int price;
     int capacity;
-  } drinks[N];
+  };
+  std::vector<Drink> drinks(N);
   for (auto &v : drinks) {
     std::cin >> v.price >> v.capacity;
   }
-  
+
+  std::vector<int> f(L+5, 0x3ffffff);
+
+  f[0]=0;
+  for (int i = 0; i < N; i++)
+    for (int j = L; j >= 0; j--)
+      f[j] = std::min(f[j], f[std::max(j - drinks[i].capacity,0)] + drinks[i].price);
+
+  if (f[L] == 0x3ffffff)
+    std::cout << "no solution";
+  else
+    std::cout << f[L];
+
+  return 0;
 }
