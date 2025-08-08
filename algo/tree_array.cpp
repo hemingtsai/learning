45ce27a47f12105274cde066cd2dd1fed72aae52
hemingtsai <rarmicrozip@outlook.com>
2025-08-08 08:51:55 +0800

    Update Code

--- algo/tree_array.cpp
+++ algo/tree_array.cpp
@@ -1,17 +1,19 @@
 #include <cstdint>
 #include <iostream>
-#include <string>
 #include <vector>
+
 constexpr int MAXN = (1 << 12) + 10; // 2^12=4096
 enum Operator { UPDATE = 1, SUM = 2 };
 int n, m;
 int64_t c[MAXN][MAXN]; // 改为int64_t更安全
+
 inline int lowbit(int i) { return i & (-i); }
 void add(const int x, const int y, const int z) {
   for (int i = x; i <= n; i += lowbit(i))
     for (int j = y; j <= m; j += lowbit(j))
       c[i][j] += z;
 }
+
 int64_t sum(const int x, const int y) {
   int64_t result = 0;
   for (int i = x; i > 0; i -= lowbit(i))
@@ -19,9 +21,11 @@ int64_t sum(const int x, const int y) {
       result += c[i][j];
   return result;
 }
+
 int64_t range_sum(const int x1, const int y1, const int x2, const int y2) {
   return sum(x2, y2) - sum(x1 - 1, y2) - sum(x2, y1 - 1) + sum(x1 - 1, y1 - 1);
 }
+
 int main() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
